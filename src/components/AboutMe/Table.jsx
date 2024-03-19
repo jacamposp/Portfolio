42e7726a4de77040a25569a38443eb9aba9fce57
Joel Campos <jcamposp10@gmail.com>
2024-03-18 23:00:03 -0600

    add job descriptions

--- src/components/AboutMe/Table.jsx
+++ src/components/AboutMe/Table.jsx
@@ -19,8 +19,12 @@ export default function Table() {
                 </div>
               </div>
               <div className={classes.dataItem}>
-                <div>Web Developer</div>
-                <div>Descripcion de puesto.</div>
+                <div className={classes.jobTitle}>Web Developer</div>
+                <div className={classes.description}>
+                  Build and maintain critical components used in 3M.com page.
+                  Work closely with agile teams to implement and advocate for
+                  best practices in web accessibility.
+                </div>
               </div>
             </div>
             <div className={classes.dataItemContainer}>
@@ -31,8 +35,11 @@ export default function Table() {
                 </div>
               </div>
               <div className={classes.dataItem}>
-                <div>Junior Developer</div>
-                <div>Descripcion de puesto.</div>
+                <div className={classes.jobTitle}>Junior Developer</div>
+                <div className={classes.description}>
+                  Led, designed and built the new email tracker tool for the
+                  company used in 3 countries.
+                </div>
               </div>
             </div>
           </div>
@@ -50,8 +57,13 @@ export default function Table() {
                 </div>
               </div>
               <div className={classes.dataItem}>
-                <div>Computer Science</div>
-                <div>Descripcion de puesto.</div>
+                <div className={classes.jobTitle}>Computer Science</div>
+                <div className={classes.description}>
+                  Designed, developed, and implemented software products for
+                  local computers, web, or mobile devices that responded to the
+                  environment's needs and international parameters of good
+                  practices, aiming to maximize business performance.
+                </div>
               </div>
             </div>
             <div className={classes.dataItemContainer}>
@@ -62,8 +74,12 @@ export default function Table() {
                 </div>
               </div>
               <div className={classes.dataItem}>
-                <div>UX/UI</div>
-                <div>Descripcion de puesto.</div>
+                <div className={classes.jobTitle}>UX Full Stack</div>
+                <div className={classes.description}>
+                  Learned why experience design and user-centered design is the
+                  right way to design, not just products and services, but to
+                  transform entire businesses and improve people's lives.
+                </div>
               </div>
             </div>
             <div className={classes.dataItemContainer}>
@@ -74,17 +90,20 @@ export default function Table() {
                 </div>
               </div>
               <div className={classes.dataItem}>
-                <div>Software Development</div>
-                <div>Descripcion de puesto.</div>
+                <div className={classes.jobTitle}>Software Development</div>
+                <div className={classes.description}>
+                  Learned to design, implement, and analyze solutions to
+                  problems that arise in everyday life - with computers.
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div>
+        {/* <div>
           <div>
             <img src={aboutmeimg} alt="Memoji apple" />
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
